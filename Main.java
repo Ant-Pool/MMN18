2d41a3d1fd7f8e654182905ff0e4a95205d003a5
William Frost <bill.frostinator@gmail.com>
2015-08-12 22:41:35 +0000

    First commit

--- Main.java
+++ Main.java
@@ -1,6 +1,20 @@
 public class Main {
-    /*public static void main(String[] args)
+    public static void main(String[] args)
     {
         System.out.println("Hello World!");
-    }*/
+        HeightTree ht = new HeightTree(5);
+        ht.inorderTreeWalk(ht.getRoot());
+        ht.rbInsert(2);
+        ht.rbInsert(7);
+        ht.rbInsert(7);
+        ht.rbInsert(8);
+        ht.rbInsert(3);
+        ht.rbInsert(10);
+        ht.rbInsert(6);
+        ht.rbInsert(4);
+        ht.rbInsert(6);
+        ht.rbInsert(6);
+        System.out.println("The new root: " + ht.getRoot().data);
+        ht.inorderTreeWalk(ht.getRoot());
+    }
 }
--- SmallSideNode.java
+++ SmallSideNode.java
@@ -1,3 +1,28 @@
 public class SmallSideNode {
-    
+    public int color;
+    public double data;
+    public SmallSideNode parent;
+    public SmallSideNode left;
+    public SmallSideNode right;
+
+    public final int RED = 0;
+    public final int BLACK = 1;
+
+
+    public SmallSideNode(double _data)
+    {
+        color = RED;
+        data = _data;
+        parent = null;
+        left = null;
+        right = null;
+    }
+    public SmallSideNode(int _data, SmallSideNode _parent, SmallSideNode _left, SmallSideNode _right)
+    {
+        color = RED;
+        data = _data;
+        parent = _parent;
+        left = _left;
+        right = _right;
+    }
 }
